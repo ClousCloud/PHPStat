b54bf437a53e7a6f4cfcd88da75264e85e708110
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-09-21 19:03:39 +0700

    Create CyclomaticComplexityRule.php

---
+++ src/rules/CyclomaticComplexityRule.php
@@ -0,0 +1,27 @@
+namespace nurazlib\phpstat\rules;
+
+use PhpParser\Node;
+use PhpParser\NodeVisitorAbstract;
+
+class CyclomaticComplexityRule extends NodeVisitorAbstract
+{
+    private $complexity = 1;
+
+    public function enterNode(Node $node)
+    {
+        // Tambah kompleksitas untuk struktur kontrol
+        if ($node instanceof Node\Stmt\If_ || 
+            $node instanceof Node\Stmt\For_ || 
+            $node instanceof Node\Stmt\Foreach_ || 
+            $node instanceof Node\Stmt\While_ || 
+            $node instanceof Node\Stmt\Switch_ || 
+            $node instanceof Node\Expr\Ternary) {
+            $this->complexity++;
+        }
+    }
+
+    public function getComplexity()
+    {
+        return $this->complexity;
+    }
+}
